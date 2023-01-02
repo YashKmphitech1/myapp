729dd79d3e53a0f98e610fe2aa0bff2280682351
Yashv Kmphitech <yashv.kmphitech@gmail.com>
2023-01-02 14:51:12 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import AddStudent from "./pages/AddStudent";
+import { Route, Routes } from "react-router-dom";
+import StudentList from "./pages/StudentList";
+import EditStudent from "./pages/EditStudent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<StudentList />} />
+        <Route path="/add" element={<AddStudent />} />
+        <Route path="/edit/:id" element={<EditStudent />} />
+      </Routes>
+    </>
   );
 }
 
